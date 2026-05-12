16947adbefac6d1e0fff24e7497f4d7b39a875b2
Henry Ogun <henryoogun@gmail.com>
2026-05-12 15:20:50 +0100

    Initial SalesTrack PWA release

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'salestrack-v1';
+const ASSETS = ['./salestrack.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  // Network first, fall back to cache (ensures fresh data when online)
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
